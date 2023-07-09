09f5fa8ef3803048492ff3d6c29045fec50cdca0
Hadi Samadzad <h.samadzad@gmail.com>
2023-07-09 17:38:25 +0100

    Clean code base

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-import { Gallery } from './components/Gallery'
-import Sidebar from './components/Sidebar'
-
-
-const App = () => {
-  const [count, setCount] = useState(0)
+function App() {
 
   return (
-    <div className="App">
-      <Sidebar />
-      <button onClick={() => setCount((count) => count + 1)}>
-        Count is {count}
-      </button>
-      <Gallery />
-    </div>
+    <>
+    </>
   )
 }
 
