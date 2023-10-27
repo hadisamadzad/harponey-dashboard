9883951657eabacf22b6a525112dfa3ef3165436
Hadi Samadzad <hadi.samadzad@sainsburys.co.uk>
2023-10-27 11:53:56 +0100

    Add navbar to the dashboard

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import Sidebar from "@/Components/Sidebar/Sidebar";
-import { semanticColors } from "@nextui-org/react";
+import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, semanticColors } from "@nextui-org/react";
 import { useState } from "react";
 
 const MainLayout: React.FC = () => {
@@ -19,7 +19,37 @@ const MainLayout: React.FC = () => {
             toggleSidebar={toggleSidebar}
           ></Sidebar>
         </aside>
-        <div className="header"></div>
+        <div className="header w-full">
+        <Navbar position="static" isBordered maxWidth="full">
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+        </div>
         <div className="main"></div>
         <div className="footer"></div>
       </div>
