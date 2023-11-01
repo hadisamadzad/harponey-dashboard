d8cb491d1781666b63f683a73aefdd89ae319870
Hadi Samadzad <hadi.samadzad@sainsburys.co.uk>
2023-11-01 00:13:55 +0000

    Fix sidebar issue

---
+++ src/Components/Sidebar.tsx
@@ -0,0 +1,181 @@
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  Button,
+  Listbox,
+  ListboxItem,
+  ListboxSection,
+} from "@nextui-org/react";
+import { GoSidebarCollapse, GoSidebarExpand } from "react-icons/go";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+
+interface SidebarProps {
+  isOpen: boolean;
+  toggleSidebar: () => void;
+}
+
+// Constants
+const logoName = "kubCheck";
+const shortLogoName = "k";
+
+// Variants
+const sidebarVariants = {
+  open: {
+    width: 320, // Expand the sidebar width when open
+  },
+  closed: {
+    width: 70, // Collapse the sidebar when closed
+  },
+};
+
+// CSS Classes
+const classSidebarContainer = "h-full  p-3"
+const classSidebarHeader = ""
+const classSidebarMenu = ""
+
+const classDividerCommon = "text-primary-300 border-t-1.5";
+const classDividerIsOpen = classDividerCommon + " border-primary-800";
+const classDividerIsClosed =
+  classDividerCommon + " border-primary-400 duration-1000 -pt-1";
+
+const menuItemClasses = "text-zinc-300 h-12 mb-2";
+const iconClasses = "text-xl text-white pointer-events-none flex-shrink-0";
+
+const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
+  return (
+    <aside className="h-full overflow-x-hidden overflow-y-auto bg-primary-800 scrollbar-hide ">
+      <motion.div
+        className={classSidebarContainer}
+        initial={isOpen ? "open" : "closed"}
+        animate={isOpen ? "open" : "closed"}
+        variants={sidebarVariants}
+      >
+        <div className={classSidebarHeader}>
+          <h2 className="text-white text-3xl font-extrabold mx-auto mt-2 mb-6">
+            <span className="text-primary-400 text-4xl">.</span>
+            {isOpen ? logoName : shortLogoName}
+          </h2>
+        </div>
+        <div className="">
+          <Listbox
+            variant="flat"
+            title="Menu"
+            aria-label="Listbox menu with descriptions"
+          >
+            <ListboxSection
+              title={isOpen ? "Actions" : " "}
+              showDivider
+              className={isOpen ? classDividerIsOpen : classDividerIsClosed}
+            >
+              <ListboxItem
+                key="dashboard"
+                startContent={<MdOutlineSpaceDashboard className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-zinc-200 ">Dashboard</span>
+              </ListboxItem>
+              <ListboxItem
+                key="copy"
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Environments</span>
+              </ListboxItem>
+              <ListboxItem
+                key="edit"
+                showDivider
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Instances</span>
+              </ListboxItem>
+            </ListboxSection>
+            <ListboxSection
+              title={isOpen ? "Actions" : " "}
+              className={isOpen ? classDividerIsOpen : classDividerIsClosed}
+            >
+              <ListboxItem
+                key="delete"
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Users</span>
+              </ListboxItem>
+
+              <ListboxItem
+                key="delete"
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Users</span>
+              </ListboxItem>
+              <ListboxItem
+                key="delete"
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Users</span>
+              </ListboxItem>
+              <ListboxItem
+                key="delete"
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Users</span>
+              </ListboxItem>
+              <ListboxItem
+                key="delete"
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Users</span>
+              </ListboxItem>
+              <ListboxItem
+                key="delete"
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Users</span>
+              </ListboxItem>
+              <ListboxItem
+                key="delete"
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Users</span>
+              </ListboxItem>
+              <ListboxItem
+                key="delete"
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Users2</span>
+              </ListboxItem>
+              <ListboxItem
+                key="delete"
+                startContent={<GoSidebarExpand className={iconClasses} />}
+                className={menuItemClasses}
+              >
+                <span className="text-white">Users3</span>
+              </ListboxItem>
+            </ListboxSection>
+          </Listbox>
+        </div>
+
+
+        <Button
+          isIconOnly
+          color="default"
+          variant="light"
+          aria-label="Open Sidebar"
+          onClick={toggleSidebar}
+          className="text-white text-2xl absolute right-4 bottom-4"
+        >
+          {isOpen ? <GoSidebarExpand /> : <GoSidebarCollapse />}
+        </Button>
+      </motion.div>
+    </aside >
+  );
+};
+
+export default Sidebar;
--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,57 +1,47 @@
-import Sidebar from "@/Components/Sidebar/Sidebar";
-import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, semanticColors } from "@nextui-org/react";
+import Footer from "@/Components/Footer";
+import Header from "@/Components/Header";
+import Sidebar from "@/Components/Sidebar";
 import { useState } from "react";
 
 const MainLayout: React.FC = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
-    console.log(semanticColors.light.primary[500]);
   };
 
   return (
     <>
-      <div className="flex flex-row h-full">
-        <aside className="overflow-x-hidden h-full">
-          <Sidebar
-            isOpen={isSidebarOpen}
-            toggleSidebar={toggleSidebar}
-          ></Sidebar>
-        </aside>
-        <div className="header w-full">
-        <Navbar position="static" isBordered maxWidth="full">
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar
+          isOpen={isSidebarOpen}
+          toggleSidebar={toggleSidebar}
+        ></Sidebar>
+
+        <div className="flex flex-col h-screen w-full justify-between">
+          <header>
+            <Header />
+          </header>
+
+          <div>
+            main
+            <p>mainmainmainmain</p>
+            <p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p>
+            <p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p>
+            <p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p>
+            <p>mainmainmainmain</p>
+            <p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p>
+            <p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p>
+            <p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p>
+            <p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p>
+            <p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p>
+            <p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p><p>mainmainmainmain</p>
+            <p>mainmainmainmain</p>
+          </div>
+
+          <Footer />
         </div>
-        <div className="main"></div>
-        <div className="footer"></div>
+
       </div>
     </>
   );
